88c667a04413de05a92c7118e633c3c7cc66e0c6
yshuman1 <yshuman@loyalloops.com>
2017-09-23 21:19:23 -0700

    pushing to heroku

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log('Server is up on port ${port}');
-  
-})
-
-
-
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
