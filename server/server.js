edb9a582455bd42b7fda829b16df20882ea3cc1a
yshuman1 <yshuman@loyalloops.com>
2017-09-26 15:42:34 -0700

    added connect and disconnet event handlers

--- server/server.js
+++ server/server.js
@@ -11,6 +11,15 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  
+  socket.on('disconnect', ()=> {
+    console.log('User was disconnected');
+    
+  })
+});
+
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
