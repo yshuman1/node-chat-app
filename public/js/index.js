fbbfdc96112ffeac07ba4cfc58de683d991e5a0e
yshuman1 <yshuman@loyalloops.com>
2017-09-26 18:33:27 -0700

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'yasin',
-    text: 'fancy pants!'
-  });
+ 
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
   
-  socket.emit('newMessage', {
-    from: 'mike@exaple.com',
-    text: 'hey. whats up?',
-    createdAt: 123
-  });
-
+ 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
     
   });
 
